73895162c4178fca5173b4bd36bc60e9d6590280
Suhayla924 <suhaylabhassan@gmail.com>
2021-10-07 22:00:31 -0700

    Update te6.js

--- te6.js
+++ te6.js
@@ -1,3 +1,9 @@
 
+/*testing if the console log is responding*/
 console.log("Hello World!");
 console.log("Hello");
+
+/*create an array for the loop*/
+  for (let i = 0; i <7; i++){
+    console.log('#',i *4);
+  }
