3fbb09a27de85e139059eb05fa3575888aa06049
Suhayla924 <suhaylabhassan@gmail.com>
2021-10-09 14:31:20 -0700

    new files

---
+++ part3.js
@@ -0,0 +1,20 @@
+/*declare "c"*/
+let c = '';
+
+/*testing if the console log is responding*/
+console.log("Hello World!");
+
+/*create an array for the loop*/
+/* make a for statment to create the array.
+"i" equals zero. If "i" is greater then 7 then;
+ means that 7 rows will print in the console log.
+ With each row 1 is added to "i"*/
+
+for (let i = 0; i < 7; i++) {
+  c = c + '#';
+  console.log(c);
+}
+
+/*After we declare the condition we make the function.
+We make "c" equal to "c" plus one hash.
+And it will repeat based on the conditions we set before.*/
