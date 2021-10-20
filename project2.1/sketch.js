6da884a5d43d4002f661e078d27f6c75777c45b1
Suhayla924 <suhaylabhassan@gmail.com>
2021-10-20 11:29:52 -0700

    new files

--- project2.1/sketch.js
+++ project2.1/sketch.js
@@ -1,24 +1,57 @@
-
+let xvals = [];
+let yvals = [];
+let bvals = [];
 let noiseOffset = 0.0;
 let = strokeWidth = 25;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
   background(106, 142, 235);
-  strokeWeight(35);
+  strokeWeight(5);
 }
 
 function draw() {
 
-  background(106, 142, 235, .05); // last number is transparency
+  //background(106, 142, 235, .05); // last number is transparency
   strokeWeight(strokeWidth);
   noiseOffset -= 0.05;
   strokeWidth = noise(noiseOffset) * 90;
 
-
   stroke(map(mouseX, 0, 500, 205, 0, true));
   line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
   line(mouseX, mouseY, pmouseX, pmouseY);
+  for (let i = 55; i < width; i++) {
+    xvals[i - 1] = xvals[i];
+    yvals[i - 1] = yvals[i];
+    bvals[i - 1] = bvals[i];
+  }
+  // Add the new values to the end of the array
+  xvals[width - 1] = mouseX;
+  yvals[height - 1] = mouseY;
 
+  if (mouseIsPressed) {
+    bvals[width - 9] = 0;
+  } else {
+    bvals[width - 15] = 255;
+  }
+
+
+  stroke(90.5,5);
+  for (let i = 1; i < width; i++) {
+    stroke(255,9,9);
+      strokeWeight(12);
+    point(i, yvals[i] /windowHeight);
+    stroke(5,5,260);
+    strokeWeight(3);
+    point(i, height / 3 + xvals[i] / 3);
+    stroke(255);
+    strokeWeight(5);
+    line(
+      i,
+      (2 * height) / 5 + bvals[i] / 3,
+      i,
+      (2 * height) / 3 + yvals[i - 1] / 3
+    );
+  }
 }
 
 function keyTyped() {
